ea8d995fd9983ba76dc19267fe1ce39856abf030
Roman Kalnytskyi <moranibaca@gmail.com>
2017-10-14 12:33:55 +0300

    Fix relation reloading after eager loading it

--- spec/config.cr
+++ spec/config.cr
@@ -1,33 +1,49 @@
-adapter = ""
+module Spec
+  @@adapter = ""
+
+  def self.adapter
+    @@adapter
+  end
+
+  def self.adapter=(v)
+    @@adapter = v
+  end
+end
+
 {% if env("DB") == "mysql" %}
   require "../src/jennifer/adapter/mysql"
-  adapter = "mysql"
+  Spec.adapter = "mysql"
 {% elsif env("DB") == "sqlite3" %}
   require "../src/jennifer/adapter/sqlite3"
-  adapter = "sqlite3"
+  Spec.adapter = "sqlite3"
 {% else %}
   require "../src/jennifer/adapter/postgres"
-  adapter = "postgres"
+  Spec.adapter = "postgres"
 {% end %}
 require "../src/jennifer"
 
-Jennifer::Config.configure do |conf|
-  # conf.logger.level = Logger::DEBUG
-  conf.logger.level = Logger::ERROR
-  conf.host = "localhost"
-  conf.adapter = adapter
-  conf.migration_files_path = "./examples/migrations"
-  conf.db = "jennifer_test"
+def set_default_configuration
+  Jennifer::Config.reset_config
+  Jennifer::Config.configure do |conf|
+    # conf.logger.level = Logger::DEBUG
+    conf.logger.level = Logger::ERROR
+    conf.host = "localhost"
+    conf.adapter = Spec.adapter
+    conf.migration_files_path = "./examples/migrations"
+    conf.db = "jennifer_test"
 
-  case adapter
-  when "mysql"
-    conf.user = ENV["DB_USER"]? || "root"
-    conf.password = ""
-  when "postgres"
-    conf.user = ENV["DB_USER"]? || "developer"
-    conf.password = ENV["DB_PASSWORD"]? || "1qazxsw2"
-  when "sqlite3"
-    conf.host = "./spec/fixtures"
-    conf.db = "jennifer_test.db"
+    case Spec.adapter
+    when "mysql"
+      conf.user = ENV["DB_USER"]? || "root"
+      conf.password = ""
+    when "postgres"
+      conf.user = ENV["DB_USER"]? || "developer"
+      conf.password = ENV["DB_PASSWORD"]? || "1qazxsw2"
+    when "sqlite3"
+      conf.host = "./spec/fixtures"
+      conf.db = "jennifer_test.db"
+    end
   end
 end
+
+set_default_configuration

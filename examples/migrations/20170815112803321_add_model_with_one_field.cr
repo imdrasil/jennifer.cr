169d5ab1f373ca99488397ab6ef6f1110d2f492c
Roman Kalnytskyi <moranibaca@gmail.com>
2018-03-10 16:40:03 +0200

    Migration version now is parsed from file name

--- examples/migrations/20170815112803321_add_model_with_one_field.cr
+++ examples/migrations/20170815112803321_add_model_with_one_field.cr
@@ -1,4 +1,4 @@
-class ModelWithOneField20170815112803321 < Jennifer::Migration::Base
+class AddModelWithOneField20170815112803321 < Jennifer::Migration::Base
   def up
     create_table(:one_field_models) do |t|
     end

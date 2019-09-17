ed6d35c9277b1cc5b795e571b73c89a22273886b
Anton Maminov <anton.linux@gmail.com>
2019-09-17 13:58:25 +0300

    fix integration test

--- .ci/test.sh
+++ .ci/test.sh
@@ -3,7 +3,7 @@
 if [ "$INTEGRATION" == '1' ]
 then
   # crystal spec spec/**/*_test.cr
-  crystal spec spec/integration/sam_test.cr & crystal spec spec/integration/concurrency_test.cr
+  crystal spec spec/integration/sam_test.cr && crystal spec spec/integration/concurrency_test.cr
 else
   ./bin/ameba && crystal spec
 fi

c9cd367f8ed607343474d567ba54f9eaebd48f05
Didactic Drunk <1479616+didactic-drunk@users.noreply.github.com>
2020-06-27 22:32:52 -0700

    [WIP] Allow Jennifer::Migration::Runner.migrate to specify an adapter.

--- src/jennifer/migration/version.cr
+++ src/jennifer/migration/version.cr
@@ -9,6 +9,17 @@ module Jennifer
         version: String
       )
 
+      @@adapter : Adapter::Base?
+
+      def self.adapter
+        @@adapter.not_nil!
+      end
+
+      def self.adapter=(adapter : Adapter::Base)
+        @@adapter = adapter
+        adapter
+      end
+
       def self.has_table?
         false
       end

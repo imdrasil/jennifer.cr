813c02b5e1e6fdfce7ab25dae9db210cb4aec0ee
Roman Kalnytskyi <moranibaca@gmail.com>
2021-06-17 12:49:54 +0300

    introduce github actions to replace travis

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -2,18 +2,6 @@
 
 set -exo pipefail
 
-old_mysql_setup()
-{
-    # Install newer MySQL
-    sudo apt-key adv --recv-key --keyserver keyserver.ubuntu.com A4A9406876FCBD3C456770C88C718D3B5072E1F5
-    wget http://dev.mysql.com/get/mysql-apt-config_0.8.1-1_all.deb
-    sudo dpkg -i mysql-apt-config_0.8.10-1_all.deb
-    sudo apt-get update -q
-    sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server
-    sudo mysql_upgrade -u root --force
-    sudo service mysql restart
-}
-
 new_mysql_setup()
 {
     MYSQL="mysql:8.0"
--- .ci/test.sh
+++ .ci/test.sh
@@ -2,7 +2,7 @@
 
 if [ "$INTEGRATION" == '1' ]
 then
-  ./bin/ameba && crystal spec spec/integration/sam_test.cr && crystal spec spec/integration/concurrency_test.cr -Dpreview_mt
+   crystal spec spec/integration/sam_test.cr && crystal spec spec/integration/concurrency_test.cr -Dpreview_mt
 fi
 
 if [ "MT" == '1' ]

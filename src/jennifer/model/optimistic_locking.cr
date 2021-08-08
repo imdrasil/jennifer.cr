584b4aeded9f25fb229f82167e46d28a5784fea0
Chao Yang <cyang05@outlook.com>
2021-08-08 18:35:20 -0500

    Add documentation for the macro

--- src/jennifer/model/optimistic_locking.cr
+++ src/jennifer/model/optimistic_locking.cr
@@ -1,5 +1,31 @@
 module Jennifer::Model
   module OptimisticLocking
+    # Add optimistic locking to model
+    # By default, it uses column `lock_version : Int32` as lock
+    # The column used as lock must be type `Int32` or `Int64` and the default value of 0
+    # You can use a different column name as lock by passing the column name to the method.
+    #
+    # ```
+    # class MyModel < Jennifer::Model::Base
+    #   with_optimistic_lock
+    #
+    #   mapping(
+    #     id: {type: Int32, primary: true},
+    #     lock_version: {type: Int32, default: 0},
+    #   )
+    # end
+    # ```
+    # Or use a custom column name for the locking column:
+    # ```
+    # class MyModel < Jennifer::Model::Base
+    #   with_optimistic_lock(custom_lock)
+    #
+    #   mapping(
+    #     id: {type: Int32, primary: true},
+    #     custom_lock: {type: Int32, default: 0},
+    #   )
+    # end
+    # ```
     macro with_optimistic_lock(locking_column = lock_version)
       def self.locking_column
         {{locking_column.stringify}}

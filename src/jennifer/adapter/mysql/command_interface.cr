63202d39e0a1ed6e9b98c620098535cc593d77ce
David A. K. Ad <3106338+Dakad@users.noreply.github.com>
2021-12-16 16:30:41 +0100

    Add missing port arg. into load_schema cmd (#395)
    Add missing port arg into load_schema cmd

--- src/jennifer/adapter/mysql/command_interface.cr
+++ src/jennifer/adapter/mysql/command_interface.cr
@@ -17,7 +17,8 @@ module Jennifer
 
       def generate_schema
         options = ["-u", config.user, "--no-data", "-h", config.host, "--skip-lock-tables", config.db]
-        options += ["--password=#{config.password}"] if !config.password.empty?
+        options += ["--password='#{config.password}'"] unless config.password.empty?
+        options += ["--port=#{config.port}"] if config.port > -1
         command = Command.new(
           executable: "mysqldump",
           options: options,
@@ -27,8 +28,9 @@ module Jennifer
       end
 
       def load_schema
-        options = ["-u", config.user, "-h", config.host, config.db, "-B", "-s", "-e"]
-        options += ["--password=#{config.password}"] if !config.password.empty?
+        options = ["-u", config.user, "-h", config.host, config.db, "-B", "-s"]
+        options += ["--password='#{config.password}'"] unless config.password.empty?
+        options += ["--port=#{config.port}"] if config.port > -1
         command = Command.new(
           executable: "mysql",
           options: options,

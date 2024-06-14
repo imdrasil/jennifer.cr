ad70e9239bf50ea334c68e5c6d3b38aad66c6c4b
Losses Don <1384036+Losses@users.noreply.github.com>
2024-06-14 14:19:41 +0800

    fix: Command cannot be executed on Windows

--- src/jennifer/adapter/command_shell/i_command_shell.cr
+++ src/jennifer/adapter/command_shell/i_command_shell.cr
@@ -3,9 +3,8 @@ require "./command"
 module Jennifer
   module Adapter
     abstract class ICommandShell
-      OPTIONS_PLACEHOLDER = %("${@}")
-      SUDO                = "sudo"
-
+      OPTIONS_PLACEHOLDER = {% flag?(:win32) ? "" : %("${@}") %}
+      SUDO                = {% flag?(:win32) ? "" : "sudo" %}
       abstract def execute(command) : NamedTuple
 
       getter config : Config
@@ -15,7 +14,13 @@ module Jennifer
 
       private def invoke(command_string, options) : NamedTuple
         io = IO::Memory.new
-        result = Process.run(command_string, options, shell: true, output: io, error: io)
+
+        result = {% if flag?(:win32) %}
+            Process.run("cmd", ["/c", command_string] + options, output: io, error: io)
+          {% else %}
+            Process.run(command_string, options, shell: true, output: io, error: io)
+          {% end %}
+
         raise Command::Failed.new(result.exit_code, io) if result.exit_code != 0
         {status: result, output: io}
       end
